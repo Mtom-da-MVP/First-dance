4294bce7cb2f2c56ca6dc9262fe89d4230e2f2b6
Mtom-Da-MVP <start.chuckred@gmail.com>
2026-08-28 01:00:07 +0700

    update

--- Minh Hung/Session3/15 cau BTVN.cs
+++ Minh Hung/Session3/15 cau BTVN.cs
@@ -30,9 +30,39 @@ namespace Ho_Minh_Hung.Session3
             Console.WriteLine($"Thue VAT (8%): {Thue:#,##0}VND");
             Console.WriteLine($"Tien dien sau thue: {giaSauThue:#,##0}VND");
         }
+        static void bai2()
+        {
+            Console.Write("Nhap chieu cao (met): ");
+            double chieuCao = double.Parse(Console.ReadLine());
+            Console.Write("Nhap can nang (kg): ");
+            double canNang = double.Parse(Console.ReadLine());
+            double BMI = canNang / Math.Pow(chieuCao,2);
+            Console.WriteLine($"Chi so BMI cua ban: {BMI:F2}");
+            if (BMI < 18.5)
+            {
+                Console.WriteLine("Phan loai suc khoe: Thieu can");
+            }
+            if (BMI >= 18.5 && BMI < 23)
+            {
+                Console.WriteLine("Phan loai suc khoe: Binh thuong");
+            }
+            if (BMI >= 23 && BMI <25)
+            {
+                Console.WriteLine("Phan loai suc khoe: Tien beo phi");
+            }
+            if (BMI >= 25)
+            {
+                Console.WriteLine("Phan loai suc khoe: Beo phi");
+            }
+            double canNangToiThieu = 18.5 * Math.Pow(chieuCao, 2);
+            double canNangToiDa = 22.9 * Math.Pow(chieuCao, 2);
+            Console.WriteLine($"Khuyen dung: Can nang ly tuong cua ban nen tu" +
+                $" {canNangToiThieu:F2}kg den {canNangToiDa:F2}kg");
+        }
         public static void Main(string[] args)
         {
             bai1();
+            bai2();
             Console.ReadKey();
         }
 
